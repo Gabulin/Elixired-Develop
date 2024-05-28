d83357a79f9108337956356feff10f16e028bd87
Gabulin <andrew_bd_github@mail.ru>
2024-05-28 22:26:33 +0700

    feedback

--- src/Pages/MainContent/MainContent.js
+++ src/Pages/MainContent/MainContent.js
@@ -10,8 +10,12 @@ const MainContent = () => {
       <h1 className='main_text'>ОБЩАЯ <br></br>ФАРМАКОЛОГИЯ <br></br>С ЛЁГКОСТЬЮ</h1>
       <p className='main_subtext'>Уроки и задачи для эффективного <br></br>запоминания фармакологии в <br></br>интерактивном режиме</p>
       <Link to="/theory">
-      <button className="trial-button">НАЧАТЬ ПРОБНЫЙ УРОК</button>
+      <button className="trial-button"><strong>НАЧАТЬ ПРОБНЫЙ УРОК</strong></button>
       </Link>
+      <div className='feedback'>
+        <a className='feedback-ref' href='https://t.me/+ioTxRYcApMxjZTYy' target="_blank"></a>
+      <button className="feedback-button"><strong>ОСТАВИТЬ ОБРАТНУЮ СВЯЗЬ</strong></button>
+      </div>
       </div>
       <div className='main-content_media-container'>
         <img className='main-content_media__img' src={logoMain}></img>
